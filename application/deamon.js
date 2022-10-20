b3a45b34fabb3a5659d22089a4e3ac014a698a71
e1on <trohimovkavad@gmail.com>
2022-10-20 16:20:28 +0300

    Deamon

---
+++ application/deamon.js
@@ -0,0 +1,29 @@
+import childProcess from "child_process";
+import config from "./config/deamon.json" assert { type: "json" };
+
+const exec = childProcess.exec;
+let process = null;
+
+const observer = () => {
+    exec("tasklist", function (err, stdout) {
+        if (stdout.includes(config.processName)) {
+            !process && forkProcess();
+        } else {
+            process && killProcess();
+        }
+    });
+};
+
+const forkProcess = () => {
+    process = childProcess.fork(config.scriptPath);
+    console.log("process is running");
+};
+
+const killProcess = () => {
+    process.kill("SIGINT");
+    process = null;
+
+    console.log("process is killed");
+};
+
+setInterval(observer, config.refreshRate);
---
+++ vrchatOscAlert.cmd
@@ -0,0 +1,1 @@
+node ./application/deamon.js
